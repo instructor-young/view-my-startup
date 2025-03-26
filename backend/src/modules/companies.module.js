5ae37e44fa9db916002acb97b6c9af09830be9ad
Yoo JinYoung <young@acadeem.io>
2025-03-26 13:30:03 +0900

    [backend] feat: seed data

--- backend/src/modules/companies.module.js
+++ backend/src/modules/companies.module.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const prisma = require("../db/prisma/client.prisma");
 
 const companiesRouter = express.Router();
+
+companiesRouter.get("/", async (req, res, next) => {
+  const companies = await prisma.company.findMany({
+    orderBy: { realInvestmentAmount: "desc" },
+  });
+
+  res.json(companies);
+});
 
 module.exports = companiesRouter;
