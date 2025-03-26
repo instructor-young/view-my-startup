7113c3cab1e191ccd682d124b2e41ef7f0040b0b
Yoo JinYoung <young@acadeem.io>
2025-03-26 16:06:01 +0900

    [frontend] chore: 일부 글자 추가

--- frontend/src/pages/StartupDetailPage/components/FundsOnViewMyStartup.jsx
+++ frontend/src/pages/StartupDetailPage/components/FundsOnViewMyStartup.jsx
@@ -1,6 +1,6 @@
 import Button from "../../../components/Button";
 
-function FundsOnViewMyStartup() {
+function FundsOnViewMyStartup({ startup }) {
   return (
     <section className="mt-8">
       <div className="flex justify-between items-center mb-6">
@@ -11,11 +11,15 @@ function FundsOnViewMyStartup() {
         <Button>기업 투자하기</Button>
       </div>
 
-      <div className="py-4 border-t border-black-100">
-        <strong className="font-bold text-xl text-white">총 200억 원</strong>
-      </div>
+      <p className="mt-[120px] text-gray-200 text-sm whitespace-pre-wrap text-center">{`아직 투자한 기업이 없어요.\n버튼을 눌러 기업에 투자해 보세요!`}</p>
 
-      {/* <Table columns={startupDetail.column} rows={startupDetail.rows} /> */}
+      {/* <div>
+        <div className="py-4 border-t border-black-100">
+          <strong className="font-bold text-xl text-white">총 200억 원</strong>
+        </div>
+
+        <Table columns={startupDetail.column} rows={startupDetail.rows} />
+      </div> */}
     </section>
   );
 }
